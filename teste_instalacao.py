4dab6f4886eaf8934841e6ef2b4d104e2cfb355a
Gustavo Sacchi <gusacchisilva@gmail.com>
2024-10-04 15:08:06 -0300

    Arquivo de teste de api

---
+++ teste_instalacao.py
@@ -0,0 +1,14 @@
+# Chat Model Documents: https://python.langchain.com/docs/integrations/chat/
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+# Carregando as variaveis de '.env'
+load_dotenv()
+
+# Chama a API do modelos da Open IA.
+model = ChatOpenAI(model="gpt-4o")
+
+# Executa a chamada ao modelo
+result = model.invoke("Este é um teste. Se você recebeu a requisição responda 'Teste OK'.")
+print(result)
